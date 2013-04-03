a43b49a919b29554fb0ecd718348661bb880f191
muriloferreira <murilo18@gmail.com>
2013-04-03 11:24:13 -0300

    Update JavaLibrayLoad.java

--- JavaLibrayLoad.java
+++ JavaLibrayLoad.java
@@ -1,4 +1,7 @@
-    // carrega biblioteca jogl
+http://blog.cedarsoft.com/2010/11/setting-java-library-path-programmatically/
+
+
+// carrega biblioteca jogl
   	System.setProperty( "java.library.path", "C:/Users/murilo.ferreira/Desktop/jogl/i586" );
 		Field fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
 		fieldSysPath.setAccessible( true );
