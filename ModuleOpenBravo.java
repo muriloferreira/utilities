3b9587d3c168f80441fadca98434bffc7b670a21
muriloferreira <murilo18@gmail.com>
2013-06-27 17:10:59 -0300

    Create ModuleOpenBravo.java

---
+++ ModuleOpenBravo.java
@@ -0,0 +1,11 @@
+Features
+
+Finance and accounting
+Purchasing Management
+Sales Management
+Inventory Management
+Project Management
+Production Management
+Point of Sale
+Business Intelligence
+And many other modules (see http://www.openbravo.com/modules-catalog)
